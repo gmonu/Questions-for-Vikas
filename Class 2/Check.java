0134b3d5e6f441127ef22039e5ac55e5cd9761f9
= <=>
2021-05-08 10:23:49 +0530

    Class 2

---
+++ Class 2/Check.java
@@ -0,0 +1,10 @@
+class Check
+{
+
+	public static void main(String[] args) 
+	{		
+		int[] b = {1,2,3,4,5};  //0, 1, 2, 3, 4
+		System.out.print(b[5]);
+	}
+
+}
---
+++ Class 2/Varibaletypes.java
@@ -0,0 +1,19 @@
+class A
+{
+
+	public static void main(String[] args) {
+			
+		int[] b = {1,2,3,4,5};
+		System.out.print(b[1]);
+	}
+
+}
+
+// class B
+// {
+// 	public void add()
+// 	{
+
+// 	}
+// }
+
